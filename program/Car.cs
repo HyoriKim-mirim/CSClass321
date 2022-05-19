6da41d1b436efe12b1ce615deeef95d0d550429f
Hyori Kim <s2010@e-mirim.hs.kr>
2022-05-19 11:34:29 +0900

    초기 커밋.

---
+++ program/Car.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace CSClass32
+{
+    class Car
+    {
+        int carNumber;
+        DateTime inTime;
+        DateTime outTime;
+
+        public void SetInTime()
+        {
+            this.inTime = DateTime.Now;
+        }
+
+        internal void SetOutTime()
+        {
+            this.outTime = DateTime.Now;
+        }
+    }
+}
---
+++ program/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSClass32
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car = new Car();
+            car.SetInTime();
+            car.SetOutTime();
+        }
+    }
+}
