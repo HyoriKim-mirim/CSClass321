29d86baa8f1bedaeacc9717d1fd9acb1aac881ac
Hyori Kim <s2010@e-mirim.hs.kr>
2022-05-20 11:17:49 +0900

    #4 클래스의 인스턴스 변수와 클래스 변수 C#은 편하게 인스턴스 변수 초기화가 가능하다!

--- program/Program.cs
+++ program/Program.cs
@@ -45,6 +45,15 @@ namespace CSClass32
             Console.WriteLine(Math.PI);
 
             Hamburger nyam = new Hamburger();
+
+            Product productA = new Product();
+
+            productA.name = "포켓몬빵";
+            productA.price = 1500; 
+            //인스턴스 변수를 생성과 동시에 초기회 (C# 고유 문법)
+            Product productB = new Product() { name = "소금빵", price = 2000 };
+            Product productC = new Product() { price = 5500, name = "당근케이크" };
+            Product productD = new Product() {name = "보름달" };
         }
     }
 }
