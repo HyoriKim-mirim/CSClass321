e471ba23f872d75dae3e8378b013f67ff931d26f
Hyori Kim <s2010@e-mirim.hs.kr>
2022-05-26 11:47:07 +0900

    #6 클래스 리스트 연습 리스트에서 항목 제거할 때 인덱스 변경 주의

--- program/Program.cs
+++ program/Program.cs
@@ -10,6 +10,7 @@ namespace CSClass32
     {
         static void Main(string[] args)
         {
+/*            
             Car car = new Car();
             car.SetInTime();
             car.SetOutTime();
@@ -21,17 +22,17 @@ namespace CSClass32
             Console.WriteLine(random.Next(10, 100));
 
             Console.WriteLine(random.NextDouble());
-            Console.WriteLine(random.NextDouble()*10);
+            Console.WriteLine(random.NextDouble() * 10);
 
-            /*List<int> list = new List<int>();
+            *//*List<int> list = new List<int>();
                         list.Add(52);
                         list.Add(273); 
                         list.Add(32); 
-                        list.Add(64);*/
+                        list.Add(64);*//*
             List<int> list = new List<int>() { 54, 273, 32, 24 };
             list.Remove(52);
 
-            foreach(var item in list)
+            foreach (var item in list)
             {
                 Console.WriteLine("Count: " + list.Count + "\titem: " + item);
             }
@@ -49,11 +50,68 @@ namespace CSClass32
             Product productA = new Product();
 
             productA.name = "포켓몬빵";
-            productA.price = 1500; 
+            productA.price = 1500;
             //인스턴스 변수를 생성과 동시에 초기회 (C# 고유 문법)
             Product productB = new Product() { name = "소금빵", price = 2000 };
             Product productC = new Product() { price = 5500, name = "당근케이크" };
-            Product productD = new Product() {name = "보름달" };
+            Product productD = new Product() { name = "보름달" };*/
+
+
+            /*
+            students.Add(new Student() { name = "함기훈", grade = 1 });
+            students.Add(new Student() { name = "김미나", grade = 2 });
+            students.Add(new Student() { name = "김다흰", grade = 2 });
+            students.Add(new Student() { name = "김민정", grade = 3 });
+            students.Add(new Student() { name = "김세린", grade = 3 });
+            students.Add(new Student() { name = "김소연", grade = 1 });
+            students.Add(new Student() { name = "김지수", grade = 1 });
+            students.Add(new Student() { name = "김지호", grade = 2 });
+            students.Add(new Student() { name = "김찬희", grade = 3 });
+            students.Add(new Student() { name = "김하늘", grade = 2 });
+            students.Add(new Student() { name = "김효리", grade = 1 });*/
+
+            List<Student> students = new List<Student>() {
+            new Student() { name = "함기훈", grade = 1 },
+            new Student() { name = "김미나", grade = 2 },
+            new Student() { name = "김다흰", grade = 2 },
+            new Student() { name = "김민정", grade = 3 },
+            new Student() { name = "김세린", grade = 3 },
+            new Student() { name = "김소연", grade = 1 },
+            new Student() { name = "김지수", grade = 1 },
+            new Student() { name = "김지호", grade = 2 },
+            new Student() { name = "김찬희", grade = 3 },
+            new Student() { name = "김하늘", grade = 2 },
+            new Student() { name = "김효리", grade = 1 }
+        };
+            /*
+                        foreach (var item in students)
+                        {
+                            if(item.grade > 2)
+                            {
+                                students.Remove(item);
+                            }
+                        }*/
+
+/*            for (int i =0; i<students.Count; i++)
+            {
+                if(students[i].grade > 2)
+                {
+                    students.RemoveAt(i);
+                    i--;
+                }
+            }*/
+
+            for(int i= students.Count -1; i>=0; i--)
+            {
+                if (students[i].grade > 2)
+                {
+                    students.RemoveAt(i);
+                }
+            }
+            foreach (var item in students)
+            {
+                Console.WriteLine(item);
+            }
         }
     }
 }
---
+++ program/Student.cs
@@ -0,0 +1,13 @@
+﻿namespace CSClass32
+{
+    internal class Student
+    {
+        internal string name;
+        internal int grade;
+
+        public override string ToString()
+        {
+            return this.name + " : " + this.grade;
+        }
+    }
+}
